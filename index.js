a8327b212510b1c355bd85d731d710d308d8af4a
Ugochukwu Benedict Nebolisa <nebolisaugochukwu@gmail.com>
2024-11-01 23:59:03 +0100

    [feat: mobile-nav]

---
+++ index.js
@@ -0,0 +1,13 @@
+const menuBar = document.querySelector(".menu-bar");
+const closeIcon = document.getElementById("close-icon");
+const navLinks = document.getElementById("nav-links");
+
+menuBar.addEventListener("click", function () {
+  navLinks.classList.remove("translate-x-full");
+  navLinks.classList.add("translate-x-0");
+});
+
+closeIcon.addEventListener("click", function () {
+  navLinks.classList.remove("translate-x-0");
+  navLinks.classList.add("translate-x-full");
+});
