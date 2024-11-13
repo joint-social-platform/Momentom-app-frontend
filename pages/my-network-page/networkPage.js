b1c49fdbd6b4f3ca7b1009724bc67d770a73b678
Ugochukwu Benedict Nebolisa <nebolisaugochukwu@gmail.com>
2024-11-13 09:55:24 +0100

    [new: videos-page]

--- pages/my-network-page/networkPage.js
+++ pages/my-network-page/networkPage.js
@@ -5,7 +5,8 @@ const close_nav_btn = document.querySelector(".fa-x");
 const navigation_link_container = document.querySelector(
   ".navigation_link_container"
 );
-
+const overlay = document.querySelector(".overlay");
+const addPostBtn = document.querySelector(".add_post");
 // ====================================
 // open side bar toggle
 const group_chat_btn = document.querySelectorAll("#group_chat_btn");
@@ -36,4 +37,14 @@ group_chat_btn.forEach((btn_click) => {
 // open side bar
 back_icon.addEventListener("click", () => {
   network_container.classList.remove("open_network_container");
+});
+
+//close modal
+overlay.addEventListener("click", function (e) {
+  if (e.target.closest(".modal")) return;
+  overlay.classList.add("hidden");
+});
+
+addPostBtn.addEventListener("click", function () {
+  overlay.classList.remove("hidden");
 });
