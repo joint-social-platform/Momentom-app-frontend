07ceacf289237abdb8113fcdf047a362a6ea0a04
Clifford-Joshua <155918426+Clifford-Joshua@users.noreply.github.com>
2024-11-15 05:44:32 +0100

    chatPage functionalities

--- pages/ChatPage/ChatPage.js
+++ pages/ChatPage/ChatPage.js
@@ -1,4 +1,17 @@
 // ============================================================
+// all open function
+
+const openFunctionality = (div, classes) => {
+  return div.classList.add(classes);
+};
+
+// ============================================================
+// all close function
+const closeFunctionality = (div, classes) => {
+  return div.classList.remove(classes);
+};
+
+// ============================================================
 // nav toggle elements used
 const nav_bar_icon = document.querySelector(".nav_toggle");
 const mobile_nav = document.querySelector(".navigation_icons");
@@ -7,10 +20,10 @@ const nav_cancel_icon = document.querySelector(".cancel_nav_toggle_icon");
 // ===========================================================
 // nav toggle functionality
 nav_bar_icon.addEventListener("click", () => {
-  mobile_nav.classList.add("open_nav");
+  openFunctionality(mobile_nav, "open_nav");
 });
 nav_cancel_icon.addEventListener("click", () => {
-  mobile_nav.classList.remove("open_nav");
+  closeFunctionality(mobile_nav, "open_nav");
 });
 
 // ===========================================================
@@ -29,17 +42,17 @@ const active_friends_list = document.querySelector(".active_friends_list");
 // open chatting page functionality
 recent_friends_chat.forEach((each_chat) => {
   each_chat.addEventListener("click", () => {
-    friends.classList.add("close_friends_list");
-    chat_container.classList.add("open_chat");
+    openFunctionality(friends, "close_friends_list");
+    openFunctionality(chat_container, "open_chat");
   });
 });
 
 // ============================================================
 // open see friends functionality
 see_friends_list.addEventListener("click", () => {
-  chatting_page.classList.add("close_chatting_page");
-  group_chatting_page.classList.add("close_chatting_page");
-  active_friends_list.classList.add("open_active_friends_list");
+  openFunctionality(chatting_page, "close_chatting_page");
+  openFunctionality(group_chatting_page, "close_chatting_page");
+  openFunctionality(active_friends_list, "open_active_friends_list");
 });
 
 // ========================================================
@@ -51,12 +64,12 @@ const go_back = document.querySelectorAll(".back_icon");
 go_back.forEach((each_back_icon) => {
   each_back_icon.addEventListener("click", () => {
     if (chatting_page.classList.contains("close_chatting_page")) {
-      chatting_page.classList.remove("close_chatting_page");
-      group_chatting_page.classList.remove("close_chatting_page");
-      active_friends_list.classList.remove("open_active_friends_list");
+      closeFunctionality(chatting_page, "close_chatting_page");
+      closeFunctionality(group_chatting_page, "close_chatting_page");
+      closeFunctionality(active_friends_list, "open_active_friends_list");
     } else {
       friends.classList.remove("close_friends_list");
-      chat_container.classList.remove("open_chat");
+      closeFunctionality(chat_container, "open_chat");
     }
   });
 });
@@ -77,17 +90,19 @@ chat_header_container.addEventListener("click", (e) => {
   if (e.target.classList.contains("chat_header_title")) {
     // chat header functionality
     chat_header_title_container.forEach((each_title) => {
-      each_title.classList.remove("active_line");
-      parent_container.classList.add("active_line");
+      closeFunctionality(each_title, "active_line");
+
+      openFunctionality(parent_container, "active_line");
     });
 
     // active chat message functionality
     active_chat_messages.forEach((active_chat) => {
       const id = active_chat.dataset.id;
       const element = document.getElementById(Id);
-      active_chat.classList.remove("active_message");
+
+      closeFunctionality(active_chat, "active_message");
       if (Id == id) {
-        element.classList.add("active_message");
+        openFunctionality(element, "active_message");
       }
     });
   }
@@ -109,10 +124,13 @@ public_room.forEach((each_public_room) => {
   each_public_room.addEventListener("click", (e) => {
     const private_active_dot = active_room_private.children[0];
     const public_active_dot = active_room_public.children[0];
-    public_active_dot.classList.add("opened_room");
-    private_active_dot.classList.remove("opened_room");
-    private_chat.classList.remove("chatting_page_open");
-    public_group_chat.classList.add("chatting_page_open");
+    openFunctionality(public_active_dot, "opened_room");
+
+    closeFunctionality(private_active_dot, "opened_room");
+
+    closeFunctionality(private_chat, "chatting_page_open");
+
+    openFunctionality(public_group_chat, "chatting_page_open");
 
     // Dynamic get user name and profile
     const target = e.currentTarget;
@@ -127,16 +145,26 @@ public_room.forEach((each_public_room) => {
       // opened chat active
 
       public_room.forEach((active_room) => {
-        const name = active_room.children[1].children[0].classList;
-        const message = active_room.children[1].children[1].classList;
-        active_room.classList.remove("currently_open_chat");
-        name.remove("opened_chat");
-        message.remove("opened_chat");
+        const name = active_room.children[1].children[0];
+        const message = active_room.children[1].children[1];
+        closeFunctionality(active_room, "currently_open_chat");
+
+        closeFunctionality(name, "opened_chat");
+
+        closeFunctionality(message, "opened_chat");
 
         if (e.currentTarget) {
-          e.currentTarget.classList.add("currently_open_chat");
-          e.currentTarget.children[1].children[0].classList.add("opened_chat");
-          e.currentTarget.children[1].children[1].classList.add("opened_chat");
+          openFunctionality(e.currentTarget, "currently_open_chat");
+
+          openFunctionality(
+            e.currentTarget.children[1].children[0],
+            "opened_chat"
+          );
+
+          openFunctionality(
+            e.currentTarget.children[1].children[1],
+            "opened_chat"
+          );
         }
       });
       // ========================================================
@@ -155,10 +183,13 @@ private_room.forEach((each_private_room) => {
   each_private_room.addEventListener("click", (e) => {
     const private_active_dot = active_room_private.children[0];
     const public_active_dot = active_room_public.children[0];
-    public_active_dot.classList.remove("opened_room");
-    private_active_dot.classList.add("opened_room");
-    private_chat.classList.add("chatting_page_open");
-    public_group_chat.classList.remove("chatting_page_open");
+    closeFunctionality(public_active_dot, "opened_room");
+
+    openFunctionality(private_active_dot, "opened_room");
+
+    openFunctionality(private_chat, "chatting_page_open");
+
+    closeFunctionality(public_group_chat, "chatting_page_open");
 
     // Dynamic get user name and profile
     const target = e.currentTarget;
@@ -174,16 +205,26 @@ private_room.forEach((each_private_room) => {
     // opened chat active
 
     private_room.forEach((active_room) => {
-      const name = active_room.children[1].children[0].classList;
-      const message = active_room.children[1].children[1].classList;
-      active_room.classList.remove("currently_open_chat");
-      name.remove("opened_chat");
-      message.remove("opened_chat");
+      const name = active_room.children[1].children[0];
+      const message = active_room.children[1].children[1];
+      closeFunctionality(active_room, "currently_open_chat");
+
+      closeFunctionality(name, "opened_chat");
+
+      closeFunctionality(message, "opened_chat");
 
       if (e.currentTarget) {
-        e.currentTarget.classList.add("currently_open_chat");
-        e.currentTarget.children[1].children[0].classList.add("opened_chat");
-        e.currentTarget.children[1].children[1].classList.add("opened_chat");
+        openFunctionality(e.currentTarget, "currently_open_chat");
+
+        openFunctionality(
+          e.currentTarget.children[1].children[0],
+          "opened_chat"
+        );
+
+        openFunctionality(
+          e.currentTarget.children[1].children[1],
+          "opened_chat"
+        );
       }
     });
 
@@ -196,9 +237,11 @@ private_room.forEach((each_private_room) => {
 // ============================================================
 // group current chat detail functionality
 current_chat_detail.addEventListener("click", () => {
-  chatting_page.classList.add("close_chatting_page");
-  group_chatting_page.classList.add("close_chatting_page");
-  active_friends_list.classList.add("open_active_friends_list");
+  openFunctionality(chatting_page, "close_chatting_page");
+
+  openFunctionality(group_chatting_page, "close_chatting_page");
+
+  openFunctionality(active_friends_list, "open_active_friends_list");
 });
 
 // ===========================================================
@@ -217,15 +260,18 @@ message_input.forEach((message_input, index) => {
 
   // send message functionality
   send_message_icon[index].addEventListener("click", () => {
+    // remove emoji container
+    closeFunctionality(emoji_container[index], "show_emoji");
+
     global_index = index;
     // send recording
     if (typing_area[index].classList.contains("show_recording")) {
-      typing_area[index].classList.remove("show_recording");
+      closeFunctionality(typing_area[index], "show_recording");
       stopRecording();
     }
 
     // if input is empty don't send message
-    if (input_value.trim() !== "") {
+    if (input_value) {
       const newMessage = document.createElement("h2");
       newMessage.className = "receiver_message";
       newMessage.textContent = input_value;
@@ -274,13 +320,12 @@ const camera_container = document.querySelector(".camera_container");
 
 // ===========================================================
 // getting access to user media camera function
-const userCamera = async () => {
+const userCamera = async (div) => {
   try {
     stream = await navigator.mediaDevices.getUserMedia({ video: true });
 
-    video_container.srcObject = stream;
-    video_container.style.display = "block";
-    console.log(stream, video_container);
+    div.srcObject = stream;
+    div.style.display = "block";
   } catch (error) {
     alert(
       "Camera access was denied or is not supported, or is was not granted permission by your brower please grant permission"
@@ -292,8 +337,8 @@ const userCamera = async () => {
 // open camera
 camera_icon.forEach((cameraIcons) => {
   cameraIcons.addEventListener("click", () => {
-    body_container.classList.add("open_camera_container");
-    userCamera();
+    openFunctionality(body_container, "open_camera_container");
+    userCamera(video_container);
   });
 });
 
@@ -311,7 +356,6 @@ const Capture_Img = () => {
   const img = document.createElement("img");
   img.src = imageUrl;
   img.className = "current_img";
-  console.log(img);
 
   // Create a download link for the image
   const link = document.createElement("a");
@@ -326,7 +370,6 @@ const Capture_Img = () => {
 // ===========================================================
 // take picture
 take_picture.addEventListener("click", () => {
-  console.log("snap");
   Capture_Img();
 });
 
@@ -335,14 +378,14 @@ take_picture.addEventListener("click", () => {
 const stopCamera = () => {
   if (stream) {
     stream.getTracks().forEach((track) => track.stop());
-    camera_container.classList.remove("open_camera_canva");
+    closeFunctionality(camera_container, "open_camera_canva");
   }
 };
 
 // ===========================================================
 // close camera
 close_camera.addEventListener("click", () => {
-  body_container.classList.remove("open_camera_container");
+  closeFunctionality(body_container, "open_camera_container");
   stopCamera();
 });
 
@@ -397,7 +440,7 @@ const Mic = async () => {
 
 mic_icon.forEach((mic_icon, index) => {
   mic_icon.addEventListener("click", () => {
-    typing_area[index].classList.add("show_recording");
+    openFunctionality(typing_area[index], "show_recording");
     Mic();
   });
 });
@@ -438,7 +481,6 @@ user_file_container.forEach((user_file, index) => {
       } catch (error) {
         console.log(error);
       }
-      console.log(newImg);
 
       chat_message_container[index].appendChild(newImg);
 
@@ -487,4 +529,184 @@ File_icon.forEach((file_icon, index) => {
     global_index = index;
     user_file_container[index].click();
   });
+});
+
+// ===========================================================
+// Emoji Call
+const Emoji_Api =
+  "https://emoji-api.com/emojis?access_key=c958b31d58f1214ab8bda485df1d4d7d1e2ad8b8";
+const emoji_container = document.querySelectorAll(".bubble-bottom");
+const emoji_icon = document.querySelectorAll(".emoji_icon");
+
+emoji_icon.forEach((emoji_icon, index) => {
+  emoji_icon.addEventListener("click", () => {
+    Emoji();
+    openFunctionality(emoji_container[index], "show_emoji");
+  });
+});
+
+const Emoji = async () => {
+  try {
+    const res = await fetch(Emoji_Api);
+    const data = await res.json();
+
+    // Emoji
+    let displayMenu = data.map(({ character }) => {
+      return `  <p class='Emoji'>${character && character}</p>`;
+    });
+    displayMenu = displayMenu.join("");
+    emoji_container.forEach((container) => {
+      container.innerHTML = displayMenu;
+    });
+  } catch (error) {
+    console.log(error);
+  }
+
+  const Emoji = document.querySelectorAll(".Emoji");
+  Emoji.forEach((emoji) => {
+    emoji.addEventListener("click", () => {
+      message_input.forEach((message) => {
+        message.value += emoji.textContent;
+        input_value += emoji.textContent;
+      });
+    });
+  });
+};
+
+// ===========================================================
+// detail icon
+const details = document.querySelectorAll(".help_icon");
+const close_detail = document.querySelectorAll(".close_detail");
+const detail_container = document.querySelectorAll(".tooltip-bottom");
+
+// open detail
+details.forEach((detail, index) => {
+  detail.addEventListener("click", () => {
+    openFunctionality(detail_container[index], "show_detail");
+  });
+});
+
+// open detail
+close_detail.forEach((close, index) => {
+  close.addEventListener("click", () => {
+    closeFunctionality(detail_container[index], "show_detail");
+  });
+});
+
+// ===========================================================
+// call icon section
+let timeinterval;
+const hours = document.querySelector(".hr");
+const seconds = document.querySelector(".secs");
+const minutes = document.querySelector(".mins");
+const cancel_call = document.querySelector(".cancel");
+const start_call_btn = document.querySelector(".start");
+const end_call_btn = document.querySelector(".end_call");
+const start_video_call = document.querySelector(".video");
+const call_icon = document.querySelectorAll(".call_icon");
+const start_call_modal = document.querySelector(".start_call");
+const video_call_container = document.querySelector(".video_call");
+const video_call_icon = document.querySelectorAll(".video_call_icon");
+const call_container = document.querySelector(".normal_call_container");
+
+// ===========================================================
+// open call modal
+call_icon.forEach((call) => {
+  call.addEventListener("click", () => {
+    openFunctionality(start_call_modal, "open_call");
+    openFunctionality(start_call_btn, "open_video_call");
+    closeFunctionality(start_video_call, "open_video_call");
+  });
+});
+
+// ===========================================================
+// close call modal
+cancel_call.addEventListener("click", () => {
+  closeFunctionality(start_call_modal, "open_call");
+  closeFunctionality(start_call_btn, "open_video_call");
+});
+
+// ===========================================================
+// start call
+start_call_btn.addEventListener("click", () => {
+  callTime();
+  getUserDetails();
+  openFunctionality(call_container, "open_call");
+  call_container.style.backgroundColor = "black";
+  closeFunctionality(start_call_modal, "open_call");
+});
+
+// ===========================================================
+// end call
+end_call_btn.addEventListener("click", () => {
+  clearTime();
+  stopCamera();
+  closeFunctionality(call_container, "open_call");
+  closeFunctionality(start_call_modal, "open_call");
+  closeFunctionality(start_call_btn, "open_video_call");
+  closeFunctionality(video_call_container, "open_video_call");
+});
+
+// ===========================================================
+// get user function
+const getUserDetails = () => {
+  const name = document.querySelector(".active_user_name").textContent;
+  const img = document.querySelector(".profile_picture").src;
+  let caller_name = document.querySelector(".caller_name");
+  let caller_img = document.querySelector(".caller_profile");
+
+  caller_name.textContent = name;
+  caller_img.src = img;
+};
+
+// ===========================================================
+// time function
+
+const callTime = () => {
+  let mins = 0;
+  let secs = 0;
+  let hrs = 0;
+  timeinterval = setInterval(() => {
+    secs += 1;
+    if (secs > 60) {
+      secs = 0;
+      mins += 1;
+    }
+    if (mins > 60) {
+      mins = 0;
+      hrs += 1;
+    }
+    seconds.textContent = realTime(secs);
+    minutes.textContent = realTime(mins);
+    hours.textContent = realTime(hrs);
+  }, 1000);
+};
+
+const clearTime = () => {
+  clearInterval(timeinterval);
+};
+
+// 00:00:00
+const realTime = (value) => {
+  return value < 10 ? "0" + value : value;
+};
+
+// ===========================================================
+// open video call modal
+video_call_icon.forEach((video_call) => {
+  video_call.addEventListener("click", () => {
+    openFunctionality(start_call_modal, "open_call");
+    openFunctionality(start_video_call, "open_video_call");
+  });
+});
+
+// ===========================================================
+// start video call
+start_video_call.addEventListener("click", () => {
+  callTime();
+  getUserDetails();
+  userCamera(video_call_container);
+  openFunctionality(call_container, "open_call");
+  call_container.style.backgroundColor = "transparent";
+  openFunctionality(video_call_container, "open_video_call");
 });
