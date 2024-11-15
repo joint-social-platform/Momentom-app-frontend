1a6bbacb9a7874fb3238a7bf209646c6c0cf3b72
My-codes <fransiscasomto@gmail.com>
2024-11-15 07:42:26 +0000

    Completed Responsive Video site

---
+++ pages/videos/homepage.js
@@ -0,0 +1,14 @@
+const sidebar = document.getElementById("sidebar");
+
+const hamburgerClick = () => {
+    if (sidebar.style.display === "block") {
+        sidebar.style.display = "none"
+    } else {
+        sidebar.style.display = "block"
+    }
+}
+
+const hamburgerClic = () => {
+    sidebar.style.display = "none"
+  
+}
