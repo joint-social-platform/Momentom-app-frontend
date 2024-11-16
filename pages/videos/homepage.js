d0bdf8ce5252d49e8e4182ca7e46c5e105aba8be
My-codes <fransiscasomto@gmail.com>
2024-11-16 02:27:24 +0000

    responsive video page edited

--- pages/videos/homepage.js
+++ pages/videos/homepage.js
@@ -1,14 +1,24 @@
+/** @format */
+
 const sidebar = document.getElementById("sidebar");
+const navigation = document.getElementById("navigation-icons");
 
 const hamburgerClick = () => {
-    if (sidebar.style.display === "block") {
-        sidebar.style.display = "none"
-    } else {
-        sidebar.style.display = "block"
-    }
-}
+  if (navigation.style.display === "flex") {
+    navigation.style.display = "none";
+    sidebar.style.display = "none";
+  } else {
+    navigation.style.display = "flex";
+    sidebar.style.display = "none";
+  }
+};
 
-const hamburgerClic = () => {
-    sidebar.style.display = "none"
-  
-}
+const arrowClick = () => {
+  if (sidebar.style.display === "flex") {
+    sidebar.style.display = "none";
+    navigation.style.display = "none";
+  } else {
+    sidebar.style.display = "flex";
+    navigation.style.display = "none";
+  }
+};
