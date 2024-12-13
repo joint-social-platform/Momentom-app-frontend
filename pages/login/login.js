f2d42ad032cb1147f698fdcb5aeb7bf3f6bbfcf8
Clifford-Joshua <155918426+Clifford-Joshua@users.noreply.github.com>
2024-12-13 06:52:19 +0100

    login to welcomePage refactor routing

--- pages/login/login.js
+++ pages/login/login.js
@@ -2,5 +2,6 @@ const formLogin = document.querySelector(".form-login");
 
 formLogin.addEventListener("submit", function (e) {
   e.preventDefault();
-  window.location.href = "../welcome-page/welcomePage.html";
+  // window.location.href = "../welcome-page/welcomePage.html";
+  window.location.href = "../welcome/index.html";
 });
