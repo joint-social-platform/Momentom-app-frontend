45e8dcc846042a85d6a00bd94e7dc06648b837d1
Clifford-Joshua <155918426+Clifford-Joshua@users.noreply.github.com>
2024-12-14 18:39:34 +0100

    new functionalities added and css updated

--- pages/my-network/networkPage.js
+++ pages/my-network/networkPage.js
@@ -1,45 +1,21 @@
 // =========================================
-// Mobile nav toggle
-const open_nav_btn = document.querySelector(".fa-bars");
-const close_nav_btn = document.querySelector(".fa-x");
-const navigation_link_container = document.querySelector(
-  ".navigation_link_container"
-);
+// Modal
 const overlay = document.querySelector(".overlay");
 const addPostBtn = document.querySelector(".add_post");
-// ====================================
-// open side bar toggle
-const group_chat_btn = document.querySelectorAll("#group_chat_btn");
-const back_icon = document.querySelector(".back_icon");
-const network_container = document.querySelector(".network_container");
 
-// ==============================
-// open nav
-open_nav_btn.addEventListener("click", () => {
-  navigation_link_container.classList.add("open_nav");
+// =========================================
+// Sidebar container
+const Nav_toggle_btn = document.querySelector(".nav_toggle_container");
+const Sidebar_container = document.querySelector(".side_bar_container");
+
+// =========================================
+// Sidebar container functionality
+Nav_toggle_btn.addEventListener("click", (e) => {
+  e.currentTarget.classList.toggle("change");
+  Sidebar_container.classList.toggle("open_side_bar_container");
 });
 
-// ==============================
-// close nav
-close_nav_btn.addEventListener("click", () => {
-  navigation_link_container.classList.remove("open_nav");
-});
-
-// =================================
-// open group chat page
-group_chat_btn.forEach((btn_click) => {
-  btn_click.addEventListener("click", () => {
-    network_container.classList.add("open_network_container");
-  });
-});
-
-// ==================================
-// open side bar
-back_icon.addEventListener("click", () => {
-  network_container.classList.remove("open_network_container");
-});
-
-//close modal
+//close modal functionality
 overlay.addEventListener("click", function (e) {
   if (e.target.closest(".modal")) return;
   overlay.classList.add("hidden");
